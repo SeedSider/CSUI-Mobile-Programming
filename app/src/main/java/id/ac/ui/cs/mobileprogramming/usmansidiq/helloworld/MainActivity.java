57c8cb8231e92c468a4a5a20a872f583e2884962
Usman Sidiq <usman.sidiq71@ui.ac.id>
2020-10-05 19:37:26 +0700

    [DONE PARTLY] Selesai implementasi override back button

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/usmansidiq/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/usmansidiq/helloworld/MainActivity.java
@@ -1,55 +1,193 @@
 package id.ac.ui.cs.mobileprogramming.usmansidiq.helloworld;
-import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Handler;
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
+import android.os.Bundle;
+import java.util.Locale;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private nameTitle title = new nameTitle();
+public class MainActivity extends Activity {
+
+    // Use seconds, running and wasRunning respectively
+    // to record the number of seconds passed,
+    // whether the stopwatch is running and
+    // whether the stopwatch was running
+    // before the activity was paused.
+
+    // Number of seconds displayed
+    // on the stopwatch.
+    private int seconds = 0;
+
+    // Is the stopwatch running?
+    private boolean running;
+
+    private boolean wasRunning;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (savedInstanceState != null) {
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // Get the previous state of the stopwatch
+            // if the activity has been
+            // destroyed and recreated.
+            seconds
+                    = savedInstanceState
+                    .getInt("seconds");
+            running
+                    = savedInstanceState
+                    .getBoolean("running");
+            wasRunning
+                    = savedInstanceState
+                    .getBoolean("wasRunning");
         }
-
-        return super.onOptionsItemSelected(item);
+        runTimer();
     }
 
-    public void changeText(View view) {
-        TextView hello = findViewById(R.id.hello_world);
-        EditText nameBox = findViewById(R.id.nameBox);
-        String name = String.valueOf(nameBox.getText());
-        String welcome = String.format("Hello, %s %s!", name, title.getTitle(name));
-        hello.setText(welcome);
+    // Save the state of the stopwatch
+    // if it's about to be destroyed.
+    @Override
+    public void onSaveInstanceState(
+            Bundle savedInstanceState)
+    {
+        savedInstanceState
+                .putInt("seconds", seconds);
+        savedInstanceState
+                .putBoolean("running", running);
+        savedInstanceState
+                .putBoolean("wasRunning", wasRunning);
+    }
+
+    // If the activity is paused,
+    // stop the stopwatch.
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    // If the activity is resumed,
+    // start the stopwatch
+    // again if it was running previously.
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    // Start the stopwatch running
+    // when the Start button is clicked.
+    // Below method gets called
+    // when the Start button is clicked.
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+
+    // Stop the stopwatch running
+    // when the Stop button is clicked.
+    // Below method gets called
+    // when the Stop button is clicked.
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+
+    // Reset the stopwatch when
+    // the Reset button is clicked.
+    // Below method gets called
+    // when the Reset button is clicked.
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 0;
+    }
+
+    // Sets the NUmber of seconds on the timer.
+    // The runTimer() method uses a Handler
+    // to increment the seconds and
+    // update the text view.
+    private void runTimer()
+    {
+
+        // Get the text view.
+        final TextView timeView
+                = (TextView)findViewById(
+                R.id.time_view);
+
+        // Creates a new Handler
+        final Handler handler
+                = new Handler();
+
+        // Call the post() method,
+        // passing in a new Runnable.
+        // The post() method processes
+        // code without a delay,
+        // so the code in the Runnable
+        // will run almost immediately.
+        handler.post(new Runnable() {
+            @Override
+
+            public void run()
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                // Format the seconds into hours, minutes,
+                // and seconds.
+                String time
+                        = String
+                        .format(Locale.getDefault(),
+                                "%d:%02d:%02d", hours,
+                                minutes, secs);
+
+                // Set the text view text.
+                timeView.setText(time);
+
+                // If running is true, increment the
+                // seconds variable.
+                if (running) {
+                    seconds++;
+                }
+
+                // Post the code again
+                // with a delay of 1 second.
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
